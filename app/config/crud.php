4ae4c80b3d2a84a2bbdb016b5ef37d1c37558b55
IslanP1 <islanpereiradeoliveira@gmail.com>
2025-09-21 15:41:06 -0300

    chore: configuração conexão com o mariadb

---
+++ app/config/crud.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'host'  =>  $_ENV['DB_HOST'],
+    'port'  =>  $_ENV['DB_PORT'],
+    'name'  =>  $_ENV['DB_DATABASE'],
+    'user'  =>  $_ENV['DB_USERNAME'],
+    'pass'  =>  $_ENV['DB_PASSWORD'],
+    'type'  =>  $_ENV['DB_TYPE'],
+    'prep'  =>  "1"
+];
